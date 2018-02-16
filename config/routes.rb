07004e8105efdad48d0eeade0d52651530d3aeb2
Ali Tavakol <ali.tavakol@gmail.com>
2018-02-17 01:03:21 +0330

    rails_apps_composer: add Bootstrap page layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+
+    root to: "users#index"
+  end
+
   root to: 'visitors#index'
   devise_for :users
   resources :users
